2b5cf6ee087eb8d213b29599b04938b646bb1c15
Hoang Nguyen <hoang.nguyen@pionero.io>
2023-10-31 16:22:21 +0700

    Revert "feat: init project"
    This reverts commit d83deb530c85022f01bb9e4e8cd62aa0d52cff40.

--- src/loaders/index.ts
+++
@@ -1,8 +0,0 @@
-import { logger } from '@/configs/logger'
-import databaseLoader from '@/loaders/database'
-
-export default async () => {
-  logger.info(`Start Database Loader`)
-  await databaseLoader()
-  logger.info(`Success Database Loader`)
-}
